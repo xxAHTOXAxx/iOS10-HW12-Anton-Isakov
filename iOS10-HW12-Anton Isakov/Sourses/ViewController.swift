a2ca5f98792af53280d56f4cb12f0f3a04185d64
Anton <xxahtoxaxx2015@gmail.com>
2023-06-22 22:37:29 +0400

    add progress bar

--- iOS10-HW12-Anton Isakov/Sourses/ViewController.swift
+++ iOS10-HW12-Anton Isakov/Sourses/ViewController.swift
@@ -9,26 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var timer = Timer()
+    private var timer = Timer()
+    private var isWorkTime = true
+    private var isStarted = true
+    private let workTimeDuration = 25.0
+    private let restTimeDuration = 5.0
+    private lazy var currentTime = workTimeDuration
    
-    
     private lazy var timerLabel: UILabel = {
         let timerLabel = UILabel()
         timerLabel.translatesAutoresizingMaskIntoConstraints = false
         timerLabel.textAlignment = .center
-        timerLabel.text = "00:25"
+        timerLabel.text = "00:\(Int(workTimeDuration))"
         timerLabel.textColor = UIColor.systemGreen
         timerLabel.font = UIFont.systemFont(ofSize: 64)
         return timerLabel
     }()
-
+    
     private lazy var button: UIButton = {
-        var isWorkTime:Bool
-        var isStarted:Bool
         let button = UIButton(type: .system)
         button.tintColor = UIColor.systemGreen
-        let pauseSymbol = UIImage(systemName: "pause", withConfiguration: UIImage.SymbolConfiguration(pointSize: 32))
-        button.setImage(pauseSymbol, for: .normal)
         let playSymbol = UIImage(systemName: "play", withConfiguration: UIImage.SymbolConfiguration(pointSize: 32))
         button.setImage(playSymbol, for: .normal)
         button.translatesAutoresizingMaskIntoConstraints = false
@@ -36,15 +36,20 @@ class ViewController: UIViewController {
         return button
     }()
 
+    private lazy var progressBarLabel: CircularProgressBarView = {
+        let progressBarLabel = CircularProgressBarView()
+        progressBarLabel.translatesAutoresizingMaskIntoConstraints = false
+        return progressBarLabel
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
         setupHierarchy()
         setupLayout()
-        startButtonTapped()
     }
 
-    // MARK: - Setups
+    // MARK: - Private functions
 
     private func setupView() {
         view.backgroundColor = .black
@@ -53,6 +58,7 @@ class ViewController: UIViewController {
     private func setupHierarchy() {
         view.addSubview(timerLabel)
         view.addSubview(button)
+        view.addSubview(progressBarLabel)
     }
 
     private func setupLayout() {
@@ -63,208 +69,136 @@ class ViewController: UIViewController {
             timerLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             timerLabel.centerYAnchor.constraint (equalTo: view.centerYAnchor),
 
-            button.topAnchor.constraint(equalTo: timerLabel.bottomAnchor, constant: height * 0.06),
+            button.topAnchor.constraint(equalTo: timerLabel.bottomAnchor, constant: height * 0.04),
             button.leftAnchor.constraint (equalTo: view.leftAnchor, constant: width * 0.13),
             button.rightAnchor.constraint (equalTo: view.rightAnchor, constant: -width * 0.13)
+            
         ])
+        progressBarLabel.center = view.center
+        progressBarLabel.createCircularPath()
+        // call the animation with circularViewDuration
+        
+        // add this view to the view controller
+    }
+    
+    private func changeLabel() {
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "mm:ss"
+        let timeInterval = TimeInterval(floatLiteral: currentTime)
+        let date = Date(timeIntervalSince1970: timeInterval)
+        let formattedString = formatter.string(from: date)
+        
+        timerLabel.text = formattedString
     }
 
     // MARK: - Actions
 
     @objc func startButtonTapped() {
-        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerAction) , userInfo: nil, repeats: true)
-//        button.isWorkTime = true
-//        if button.isWorkTime == true {
-//            button.setImage(playSymbol, for: .normal)
-//        } else { button.setImage(pauseSymbol, for: .normal)}
         
-    }
-
-    @objc func changeLabel() {
-        
-        let formatter = DateFormatter()
-        formatter.dateFormat = "mm:ss"
-        let date = Date(timeIntervalSince1970: 0)
-        let formattedString = formatter.string(from: date)
-        
-        timerLabel.text = formattedString
-        
-        
+        guard isStarted else {
+            button.setImage(UIImage(systemName: "play", withConfiguration: UIImage.SymbolConfiguration(pointSize: 32)), for: .normal)
+            timer.invalidate()
+            progressBarLabel.progressAnimation(duration: currentTime)
+            isStarted.toggle()
+            return
+        }
+        button.setImage(UIImage(systemName: "pause", withConfiguration: UIImage.SymbolConfiguration(pointSize: 32)), for: .normal)
+        timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(timerAction), userInfo: nil, repeats: true)
+        progressBarLabel.progressAnimation(duration: currentTime)
+        isStarted.toggle()
     }
     
     
-    @objc  func timerAction() {
-
-
+    @objc func timerAction() {
+        
+        changeLabel()
+        
+        if currentTime <= 0 {
+            
+            if isWorkTime {
+                currentTime = restTimeDuration
+                timerLabel.textColor = UIColor.systemRed
+                button.tintColor = UIColor.systemRed
+            } else {
+                currentTime = workTimeDuration
+                timerLabel.textColor = UIColor.systemGreen
+                button.tintColor = UIColor.systemGreen
+            }
+            isWorkTime.toggle()
+        }
+        currentTime -= 0.01
     }
+    
+    // MARK: - Properties -
+        
+//        var circularProgressBarView: CircularProgressBarView!
+//var circularViewDuration: TimeInterval = 2
+//
+//    func setUpCircularProgressBarView() {
+//            // set view
+//            circularProgressBarView = CircularProgressBarView(frame: .zero)
+//            // align to the center of the screen
+//            circularProgressBarView.center = view.center
+//            // call the animation with circularViewDuration
+//            circularProgressBarView.progressAnimation(duration: circularViewDuration)
+//            // add this view to the view controller
+            
+       // }
+}
+// ProgressBar
+
+class CircularProgressBarView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    // MARK: - Properties -
+    
+    private var circleLayer = CAShapeLayer()
+    private var progressLayer = CAShapeLayer()
+    private var startPoint = CGFloat(-Double.pi / 2)
+    private var endPoint = CGFloat(3 * Double.pi / 2)
+    
+    func createCircularPath() {
+            // created circularPath for circleLayer and progressLayer
+            let circularPath = UIBezierPath(arcCenter: CGPoint(x: frame.size.width / 2.0, y: frame.size.height / 2.0), radius: 140, startAngle: startPoint, endAngle: endPoint, clockwise: true)
+            // circleLayer path defined to circularPath
+            circleLayer.path = circularPath.cgPath
+            // ui edits
+            circleLayer.fillColor = UIColor.clear.cgColor
+            circleLayer.lineCap = .round
+            circleLayer.lineWidth = 20.0
+            circleLayer.strokeEnd = 1.0
+            circleLayer.strokeColor = UIColor.white.cgColor
+            // added circleLayer to layer
+            layer.addSublayer(circleLayer)
+            // progressLayer path defined to circularPath
+            progressLayer.path = circularPath.cgPath
+            // ui edits
+            progressLayer.fillColor = UIColor.clear.cgColor
+            progressLayer.lineCap = .round
+            progressLayer.lineWidth = 10.0
+            progressLayer.strokeEnd = 0
+            progressLayer.strokeColor = UIColor.green.cgColor
+            // added progressLayer to layer
+            layer.addSublayer(progressLayer)
+        }
+    
+    func progressAnimation(duration: TimeInterval) {
+            // created circularProgressAnimation with keyPath
+            let circularProgressAnimation = CABasicAnimation(keyPath: "strokeEnd")
+            // set the end time
+            circularProgressAnimation.duration = duration
+            circularProgressAnimation.toValue = 1.0
+            circularProgressAnimation.fillMode = .forwards
+            circularProgressAnimation.isRemovedOnCompletion = false
+            progressLayer.add(circularProgressAnimation, forKey: "progressAnim")
+        }
 }
 
-//
-//
-//
-    
-//import UIKit
-//
-//class ViewController: UIViewController {
-//
-//    let timerLabel: UILabel = {
-//        let label = UILabel()
-//        label.font = UIFont.systemFont(ofSize: 48)
-//        label.textAlignment = .center
-//        return label
-//    }()
-//
-//    let playPauseButton: UIButton = {
-//        let button = UIButton(type: .system)
-//        button.setTitle("Play", for: .normal)
-//        button.setTitleColor(.systemBlue, for: .normal)
-//        button.titleLabel?.font = UIFont.systemFont(ofSize: 24)
-//        button.addTarget(self, action: #selector(playPauseButtonTapped), for: .touchUpInside)
-//        return button
-//    }()
-//
-//    let progressBar: CircularProgressView = {
-//        let view = CircularProgressView()
-//        view.trackColor = .systemGray
-//        view.progressColor = .systemBlue
-//        return view
-//    }()
-//
-//    var isWorkTime = true
-//    var isStarted = false
-//
-//    var timer: Timer?
-//    var totalTime: TimeInterval = 25
-//    var currentTime: TimeInterval = 0
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//
-//        view.backgroundColor = .white
-//
-//        view.addSubview(timerLabel)
-//        view.addSubview(playPauseButton)
-//        view.addSubview(progressBar)
-//
-//        timerLabel.translatesAutoresizingMaskIntoConstraints = false
-//        timerLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-//        timerLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-//        timerLabel.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.8).isActive = true
-//
-//        playPauseButton.translatesAutoresizingMaskIntoConstraints = false
-//        playPauseButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-//        playPauseButton.topAnchor.constraint(equalTo: timerLabel.bottomAnchor, constant: 20).isActive = true
-//
-//        progressBar.translatesAutoresizingMaskIntoConstraints = false
-//        progressBar.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-//        progressBar.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-//        progressBar.widthAnchor.constraint(equalToConstant: 200).isActive = true
-//        progressBar.heightAnchor.constraint(equalToConstant: 200).isActive = true
-//
-//        updateUI()
-//    }
-//
-//    func updateUI() {
-//        let formattedTime = formatTime(currentTime)
-//        timerLabel.text = formattedTime
-//
-//        let playPauseButtonTitle = isStarted ? (isWorkTime ? "Pause" : "Play") : "Play"
-//        playPauseButton.setTitle(playPauseButtonTitle, for: .normal)
-//
-//        let progress = CGFloat(currentTime / totalTime)
-//        progressBar.setProgress(progress)
-//
-//        let progressBarColor = isWorkTime ? UIColor.red : UIColor.green
-//        progressBar.setProgressColor(progressBarColor)
-//    }
-//
-//    @objc func playPauseButtonTapped() {
-//        isStarted = !isStarted
-//
-//        if isStarted {
-//            startTimer()
-//        } else {
-//            stopTimer()
-//        }
-//
-//        updateUI()
-//    }
-//
-//    func startTimer() {
-//        if timer == nil {
-//            timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
-//        }
-//    }
-//
-//    func stopTimer() {
-//        timer?.invalidate()
-//        timer = nil
-//    }
-//
-//    @objc func updateTimer() {
-//        currentTime += 1
-//
-//        if currentTime >= totalTime {
-//            isWorkTime.toggle()
-//            currentTime = 0
-//        }
-//
-//        updateUI()
-//    }
-//
-//    func formatTime(_ time: TimeInterval) -> String {
-//        let minutes = Int(time) / 60
-//        let seconds = Int(time) % 60
-//        return String(format: "%02d:%02d", minutes, seconds)
-//    }
-//}
-//
-//class CircularProgressView: UIView {
-//    var trackColor: UIColor = .lightGray
-//    var progressColor: UIColor = .blue
-//    private var progressLayer = CAShapeLayer()
-//
-//    override init(frame: CGRect) {
-//        super.init(frame: frame)
-//        configureProgressLayer()
-//    }
-//
-//    required init?(coder aDecoder: NSCoder) {
-//        super.init(coder: aDecoder)
-//        configureProgressLayer()
-//    }
-//
-//    private func configureProgressLayer() {
-//        let center = CGPoint(x: bounds.midX, y: bounds.midY)
-//        let radius = min(bounds.width, bounds.height) / 2
-//
-//        let circularPath = UIBezierPath(arcCenter: center, radius: radius, startAngle: -.pi / 2, endAngle: 3 * .pi / 2, clockwise: true)
-//
-//        progressLayer.path = circularPath.cgPath
-//        progressLayer.strokeColor = progressColor.cgColor
-//        progressLayer.lineWidth = 10
-//        progressLayer.fillColor = UIColor.clear.cgColor
-//        progressLayer.lineCap = .round
-//        progressLayer.strokeEnd = 0
-//
-//        layer.addSublayer(progressLayer)
-//
-//        let trackLayer = CAShapeLayer()
-//        trackLayer.path = circularPath.cgPath
-//        trackLayer.strokeColor = trackColor.cgColor
-//        trackLayer.lineWidth = 10
-//        trackLayer.fillColor = UIColor.clear.cgColor
-//
-//        layer.addSublayer(trackLayer)
-//    }
-//
-//    func setProgress(_ progress: CGFloat) {
-//        progressLayer.strokeEnd = progress
-//    }
-//
-//    func setProgressColor(_ color: UIColor) {
-//        progressLayer.strokeColor = color.cgColor
-//    }
-//}
-//
-//
